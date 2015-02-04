b0e813f79fe255934b01c9aaa2f77c296c4ebf97
Ha Son Hai <hasonhai124@gmail.com>
2015-02-04 17:02:16 +0100

    fix bugs for autorun on baremetal

--- measurement_script/schedulebm.sh
+++ measurement_script/schedulebm.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 appdir="/home/sonhai/io_measurements/measurement_script/"
-for drive in sda sdg sdh sdi sdj;
+
+if [ "$1" = "" ]; then
+    oldtestdir="/mnt/sda/fiodata"
+else oldtestdir="$1"
+fi
+
+for drive in sdg sdh sdi sdj;
 do
-    sed -i "s|/home/sonhai/fiodata|/mnt/${drive}/fiodata|g" $appdir/testdir/*.fio 
-    $appdir/bigfootprocs16test.sh
+    newtestdir="/mnt/${drive}/fiodata"
+    echo "Doing test at drive $drive"
+    sed -i "s|${oldtestdir}|${newtestdir}|g" $appdir/testdir/*.fio 
+    $appdir/bigfoot16procstest.sh
     mv $appdir/BigfootSeq* $appdir/procs16_${drive}/
+    oldtestdir="$newtesdir"
 done
