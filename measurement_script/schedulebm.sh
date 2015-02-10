27b1d5e0ae7082e5aedfbde4bf8b31717a70218b
Ha Son Hai <hasonhai124@gmail.com>
2015-02-10 14:23:41 +0100

    add data for baremetal on lvm

--- measurement_script/schedulebm.sh
+++ measurement_script/schedulebm.sh
@@ -2,16 +2,16 @@
 appdir="/home/sonhai/io_measurements/measurement_script/"
 
 if [ "$1" = "" ]; then
-    oldtestdir="/mnt/sdh/fiodata"
+    oldtestdir="/mnt/sda/fiodata"
 else oldtestdir="$1"
 fi
 
-for drive in sdi sdj;
+for drive in sda sdf sdg sdh sdi sdj;
 do
     newtestdir="/mnt/${drive}/fiodata"
     echo "Doing test at drive $drive"
     sed -i "s|${oldtestdir}|${newtestdir}|g" $appdir/testdir/*.fio 
     $appdir/bigfoot16procstest.sh
-    mv $appdir/BigfootSeq* $appdir/procs16_${drive}/
+    mv $appdir/BigfootSeq* $appdir/procs16_${drive}_lvm/
     oldtestdir="$newtestdir"
 done
